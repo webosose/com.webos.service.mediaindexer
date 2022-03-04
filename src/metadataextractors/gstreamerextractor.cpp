581821435ab5eef46bd5297fcafaa1e02b62085e
paramesh.kuberappa <paramesh.kuberappa@lge.com>
2022-03-04 21:00:34 +0530

    Fix Thumbnail generation issue in OSE
    :Release Notes:
    Implement fix for Thumbnail issue in OSE
    
    :Detailed Notes:
    Thumbnails were not generated for some of the Videos in USB and hence
    Thumbnails are showing empty as without Image and Title.
    Extraction of Metadata failed for some files due to less timeout,
    Hence Increased the timeout value for gst discoverer
    
    :Testing Performed:
    Verified Locally
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRN-17972] Analyze And Fix thumbnail issue in OSE
    
    Change-Id: I1eedbf4ab8b2c99feee3083e9e86266e336374fa
    Reviewed-on: http://gpro.lge.com/c/webosose/com.webos.service.mediaindexer/+/327100
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Paramesh Kuberappa <paramesh.kuberappa@lge.com>
    Reviewed-by: <vijay.goudnaikar@lge.com>
    Reviewed-by: <kalaimani.k@lge.com>
    Tested-by: Paramesh Kuberappa <paramesh.kuberappa@lge.com>

--- src/metadataextractors/gstreamerextractor.cpp
+++ src/metadataextractors/gstreamerextractor.cpp
@@ -75,7 +75,8 @@ GStreamerExtractor::~GStreamerExtractor()
 bool GStreamerExtractor::extractMeta(MediaItem &mediaItem, bool extra) const
 {
     bool ret = true;
-    GstDiscoverer *discoverer = gst_discoverer_new(GST_SECOND, NULL);
+    //Some files may require more than one second for metadata extraction. Fix for WRN-15587
+    GstDiscoverer *discoverer = gst_discoverer_new(3 * GST_SECOND, NULL);
     GError *error = nullptr;
 
     if (!discoverer) {
